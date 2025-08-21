458a66364075f681ac87b0040a61a0b6ffb521e4
daniel0825 <149073202+daniel0285@users.noreply.github.com>
2025-08-21 09:31:37 +0800

    change email in config.php

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 
 // Gmail SMTP Configurations
-define('SMTP_EMAIL', 'sisfinance3220@gmail.com');
+define('SMTP_EMAIL', 'soliera.logistic2@gmail.com');
 define('SMTP_NAME', 'Soliera Logistic 2');
-define('SMTP_PASSWORD', 'btwh nbqe uwss ipsq'); // Gmail app password
+define('SMTP_PASSWORD', 'feow wuid yrxp hagx'); // Gmail app password
