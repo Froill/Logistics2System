3f3d34ba9cfc75f0aa9af74e5c98030fd03ad190
Froiiii <cringey.ch@gmail.com>
2026-01-23 22:06:28 +0800

    updated database

--- sql/2026_01_23_vehicle_archiving.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `fleet_vehicles`
-  ADD COLUMN `is_archived` TINYINT(1) NOT NULL DEFAULT 0,
-  ADD COLUMN `archived_at` DATETIME NULL DEFAULT NULL,
-  ADD COLUMN `archived_by` INT NULL DEFAULT NULL;
-
-CREATE INDEX `idx_fleet_vehicles_archived` ON `fleet_vehicles` (`is_archived`);
